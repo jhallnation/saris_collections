5ac6a152541d77262f6a7328c0cf734cc75e721a
Jason Hall <jhallnation@gmail.com>
2022-04-14 14:02:27 -0600

    updated nav dropdown menu

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,16 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "trix"
 import "@rails/actiontext"
+
+window.toggleMenu = function(event) {
+  event.preventDefault();
+  let dropdown = document.getElementById('dropdown-content');
+  console.log(dropdown.style);
+  if (dropdown.style.display == 'block') {
+    dropdown.style.display = '';
+    event.target.innerHTML = '<i class="material-icons md-36">menu</i>';
+  } else {
+    dropdown.style.display =  'block';
+    event.target.innerHTML = '<i class="material-icons md-36">close</i>';
+  }
+}
